d53d1845dd1b3c3cabea2702a8993112937c7b3c
Eugene Pogrebnyak <emcifuntik@gmail.com>
2018-05-17 14:01:11 +0300

    Updated to VS2017 Fixed release tools

--- Release Tools/installer_generator/build_install_script.cpp
+++ Release Tools/installer_generator/build_install_script.cpp
@@ -1,22 +1,24 @@
 #include <stdio.h>
-
 #include <iostream>
-
 #include <string>
-
 #include <string.h>
-
 #include <fstream>
-
-#include <sstream>
-
-
 
 int main(int argc, char ** argv)
 
 {
+	const char* version = "nightly";
 	std::ifstream fin("installer_template.nsi");
+	if (!fin.good()) {
+
+		std::cout << "installer_template.nsi not found";
+		return 0;
+	}
 	std::ofstream fout("installer.nsi");
+	if (!fout.good()) {
+		std::cout << "Can\'t open installer.nsi for writing";
+		return 0;
+	}
 	std::string line;
 
 	while(std::getline(fin, line))
@@ -24,12 +26,10 @@ int main(int argc, char ** argv)
 		const int pos = line.find("${VERSION}");
 
 		if(pos != -1)
-			line.replace(pos, strlen("${VERSION}"), argv[1]);
-
+			line.replace(pos, strlen("${VERSION}"), argc > 1 ? argv[1] : version);
 
 		fout << line << "\n";
-
 	}
-
-
+	fin.close();
+	fout.close();
 }
